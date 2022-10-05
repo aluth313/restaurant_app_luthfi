855c5705bc24aa9a10ec64c3fe70c41782a14191
luthfi <luthfiahmad36@gmail.com>
2022-10-05 09:46:24 +0700

    done favourite

--- lib/ui/favourite_page.dart
+++ lib/ui/favourite_page.dart
@@ -3,6 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:restaurant_app/common/styles.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
 import 'package:restaurant_app/provider/favourite_provider.dart';
+import 'package:restaurant_app/provider/restaurant_provider.dart';
+import 'package:restaurant_app/ui/detail_restaurant.dart';
+import 'package:restaurant_app/utils/result_state.dart';
 import 'package:restaurant_app/widget/restaurant_card.dart';
 
 class FavouritePage extends StatelessWidget {
@@ -32,28 +35,36 @@ class FavouritePage extends StatelessWidget {
                     height: 15,
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: 10,
-                      // physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return RestaurantCard(
-                          restaurantElement: RestaurantElement(
-                            id: 'rqdv5juczeskfw1e867',
-                            name: 'Melting Pot',
-                            description:
-                                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
-                            pictureId: '14',
-                            city: 'Medan',
-                            rating: 4.5,
-                          ),
-                          onTap: () {
-                            // state.detailRestaurant(
-                            //     state.result.restaurants[index].id);
-                            // Navigator.pushNamed(
-                            //     context, RestaurantDetail.routeName);
-                          },
-                        );
+                    child: Consumer<FavouriteProvider>(
+                      builder: (context, provider, child) {
+                        if (provider.state == ResultState.hasData) {
+                          return ListView.builder(
+                            itemCount: provider.restaurants.length,
+                            // physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return RestaurantCard(
+                                restaurantElement: provider.restaurants[index],
+                                onTap: () {
+                                  final getDetailRestaurant =
+                                      Provider.of<RestaurantProvider>(context,
+                                          listen: false);
+                                  getDetailRestaurant.detailRestaurant(
+                                      provider.restaurants[index].id);
+                                  Navigator.pushNamed(
+                                      context, RestaurantDetail.routeName);
+                                },
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(
+                            child: Text(
+                              provider.message,
+                              style: customStyleText,
+                            ),
+                          );
+                        }
                       },
                     ),
                   ),
